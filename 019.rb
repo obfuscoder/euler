ccb6625ee089ec1e3eb847fa1c344c08a2f63f20
Obfuscoder <obfuscoder@obfusco.de>
2013-02-02 20:49:48 +0100

    slightly better solution for 19

--- 019.rb
+++ 019.rb
@@ -1,28 +1,17 @@
 class Fixnum
-  BASE_YEAR = 1900
-  MONTH_DAYS = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+  MONTH_DAYS = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31].freeze
+  MONTH_DAYS_LEAP = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31].freeze
 
   def leap?
     (self%4==0 && self%100!=0) || self%400==0
   end
 
-  def days_in_year
-    leap? ? 366 : 365
-  end
-
-  def days_in_month(month)
-    month == 2 && leap? ? 29 : MONTH_DAYS[month-1]
-  end
-
-  def sundays
-    num = 0
-    days = self > BASE_YEAR ? BASE_YEAR.upto(self-1).map(&:days_in_year).reduce(:+) : 0
-    1.upto(12).each {|month|
-      num += 1 if days%7 == 6
-      days += days_in_month(month)
-    }
-    num
+  def days
+    leap? ? MONTH_DAYS_LEAP : MONTH_DAYS
   end
 end
 
-p (1901..2000).map(&:sundays).reduce(:+)
+s = 0
+d = 365
+(1901..2000).each { |y| y.days.each { |m| s += 1 if d%7 == 6; d+= m } }
+p s

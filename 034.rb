ae14f8077b4052517b2a435f616276e509d5635f
Obfuscoder <obfuscoder@obfusco.de>
2013-02-06 22:57:59 +0100

    034 condensed version

--- 034.rb
+++ 034.rb
@@ -1,18 +1,2 @@
-class Fixnum
-  @@factorials = [1,1,2,6,24,120,720,5040,40320,362880]
-
-  def digit_factorial
-    @@factorials[self]
-  end
-
-  def factorial_sum
-    self.to_s.chars.reduce(0){|m,o| m+o.to_i.digit_factorial}
-  end
-
-  def curios?
-    self == factorial_sum
-  end
-end
-
-
-p 3.upto(100000).select(&:curios?).reduce(:+)
+f = [1,1,2,6,24,120,720,5040,40320,362880]
+p 3.upto(100000).select{|n| n == n.to_s.chars.reduce(0){|m,o| m+f[o.to_i]}}.reduce(:+)

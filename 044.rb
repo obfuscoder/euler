c0ce127fcdb70b48b846d494714d8a6fee61ab91
Obfuscoder <obfuscoder@obfusco.de>
2013-02-10 23:15:45 +0100

    044

---
+++ 044.rb
@@ -0,0 +1,22 @@
+def pent(n)
+  n*(3*n-1)/2
+end
+
+def pent?(n)
+  x = ((24*n + 1)**0.5 + 1) / 6
+  x == x.to_i
+end
+
+k = 1
+while true
+  p k
+  pk = pent(k)
+  (k-1).downto(1) do |j|
+    pj = pent(j)
+    if pent?(pk - pj) && pent?(pk + pj)
+      p pk -pj
+      exit
+    end
+  end
+  k += 1
+end

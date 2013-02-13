0e0fab3d52d58053100ac4c3958e2cf013d60e39
Obfuscoder <obfuscoder@obfusco.de>
2013-02-13 14:29:33 +0100

    057

--- 057.rb
+++ 057.rb
@@ -1,6 +1,6 @@
-def f(n0,n1) 2*n1+n0 end
+f = lambda{|(n0,n1)| 2*n1+n0}
 q = [[1,0], [1,1]]
-p 1.upto(1000).count {
-  (q << [f(q.first.first, q.last.first), f(q.first.last, q.last.last)]).shift
+p (1..1000).count {
+  (q << [f.call(q.map(&:first)), f.call(q.map(&:last))]).shift
   q.last.first.to_s.length > q.last.last.to_s.length
 }

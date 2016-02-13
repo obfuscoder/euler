672294908bea6da5c8814df5ecc82703bc4969a2
Obfuscoder <obfuscoder@obfusco.de>
2016-02-13 15:32:40 +0100

    Shorten solution to problem 3

--- 003.rb
+++ 003.rb
@@ -1,11 +1,8 @@
 def divide(x)
-    (2).upto(x/2) do |i|
-        if x%i == 0
-            puts i
-            return divide(x/i)
-        end
-    end
-    x
+  (2).upto(x/2) do |i|
+    return divide(x/i) if x%i == 0
+  end
+  x
 end
 
 puts divide(600851475143)

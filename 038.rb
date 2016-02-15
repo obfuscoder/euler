912c5a37ee8aad3332447f79b31fda95a25b54b2
Obfuscoder <obfuscoder@obfusco.de>
2016-02-15 19:41:21 +0100

    Update solutions to problem 38 and 49

--- 038.rb
+++ 038.rb
@@ -1,18 +1,1 @@
-class String
-  def pandigital_product?
-    1.upto(4) do |len|
-      s = String::new(self)
-      number = s.slice!(0, len).to_i
-      2.upto(6) do |mul|
-        return true if s.length == 0
-        prod = (number*mul).to_s
-        break if s.slice!(0, prod.length) != prod
-      end
-    end
-    false
-  end
-end
-
-p '987654321'.chars.to_a.permutation.map(&:join).find {|x| x.pandigital_product?}
-
 p 9999.downto(9182).map{|n| n*10**5+2*n}.find{|n| n.to_s.chars.to_a.sort.join == '123456789'}
--- 049.rb
+++ 049.rb
@@ -3,5 +3,5 @@ require 'prime'
 '123456789'.chars.to_a.repeated_combination(4).each do |s|
   a = s.permutation.to_a.map(&:join).map(&:to_i).select(&:prime?)
   seq = a.combination(3).find{|e| e[1]-e[0] != 0 && e[2]-e[1] == e[1]-e[0]}
-  p seq.join if seq != nil
+  p seq.join.to_i if seq != nil
 end
